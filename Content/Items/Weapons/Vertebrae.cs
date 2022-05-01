925fbb9b7baedcc779ddf7b545a2c97288321d46
acats <33098659+FakeACat@users.noreply.github.com>
2022-05-01 17:41:28 +0100

    Started adding Vertebrae Whip

---
+++ Content/Items/Weapons/Vertebrae.cs
@@ -0,0 +1,17 @@
+﻿using ChaoticUprising.Content.Rarities;
+using Terraria;
+using Terraria.Enums;
+using Terraria.ModLoader;
+
+namespace ChaoticUprising.Content.Items.Weapons
+{
+    public class Vertebrae : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToWhip(915, 250, 5f, 8f, 20);
+            Item.value = Item.sellPrice(0, 30);
+            Item.rare = ModContent.RarityType<VeryEarlyChaos>();
+        }
+    }
+}
