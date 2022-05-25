446b6f6298611c20ff3625708197ea95dfff84f7
acats <33098659+FakeACat@users.noreply.github.com>
2022-05-25 18:44:43 +0100

    Added Abyssal Chaos music box

---
+++ Content/Items/Placeables/AbyssalChaosMusicBox.cs
@@ -0,0 +1,29 @@
+﻿using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChaoticUprising.Content.Items.Placeables
+{
+    public class AbyssalChaosMusicBox : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Music Box (Abyssal Chaos)");
+            Tooltip.SetDefault("Hellish Intent by ENNWAY");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Assets/Music/Hellish Intent"), ModContent.ItemType<AbyssalChaosMusicBox>(), ModContent.TileType<Tiles.Furniture.AbyssalChaosMusicBox>());
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.AbyssalChaosMusicBox>(), 0);
+            Item.width = 24;
+            Item.height = 26;
+            Item.rare = ItemRarityID.Orange;
+            Item.value = 110000;
+            Item.accessory = true;
+            Item.maxStack = 1;
+        }
+    }
+}
---
+++ Content/Tiles/Furniture/AbyssalChaosMusicBox.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader;
+
+namespace ChaoticUprising.Content.Tiles.Furniture
+{
+    public class AbyssalChaosMusicBox : AbstractMusicBox
+    {
+        public override int ItemType() => ModContent.ItemType<Items.Placeables.AbyssalChaosMusicBox>();
+    }
+}
