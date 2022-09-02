d6c08b968e826afbba854c8cc55b84ed124cf5de
acats <33098659+FakeACat@users.noreply.github.com>
2022-09-02 15:50:32 +0100

    Global enemy spawns now increase with difficulty

---
+++ Common/GlobalNPCs/ChaosModeChanges.cs
@@ -0,0 +1,22 @@
+﻿using ChaoticUprising.Common.Systems;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ChaoticUprising.Common.GlobalNPCs
+{
+    public class ChaosModeChanges : GlobalNPC
+    {
+        public override bool InstancePerEntity => true;
+
+        public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
+        {
+            if (ChaosMode.chaosMode)
+            {
+                int difficulty = (int)ChaosMode.GetDifficulty();
+                spawnRate = (int)(spawnRate * Math.Pow(0.8d, difficulty));
+                maxSpawns = (int)(maxSpawns * Math.Pow(1.2d, difficulty));
+            }
+        }
+    }
+}
